96907a07b80124cdc751c4136fb015281005b154
mahinkhankishizade <mahinkhankishizade@gmail.com>
2017-11-04 00:59:41 +0300

    Pause Menu needs to return to the Game

--- src/PauseMenu.java
+++ src/PauseMenu.java
@@ -1,5 +1,7 @@
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
@@ -13,7 +15,7 @@ import javafx.stage.Stage;
 
 import java.util.ArrayList;
 
-public class PauseMenu {
+public class PauseMenu implements EventHandler<ActionEvent> {
 
     // variables
     private static final int SETTINGS_WINDOW_WIDTH = 600;
@@ -40,13 +42,51 @@ public class PauseMenu {
         StackPane pauseLayout = new StackPane();
 
         volume = 100;
-        //volumeBar;
+        initSlider();
+        volumeBar.valueProperty().addListener(new ChangeListener<Number>() {
+            public void changed(ObservableValue<? extends Number> ov,
+                                Number old_val, Number new_val) {
+                int new_value = new_val.intValue();
+                changeVolume( new_value);
+            }
+        });
 
+        returnButton = new Button("Return");
+        backToMenu = new Button("Back to Menu");
+        returnButton.setOnAction(this);
+        backToMenu.setOnAction( event -> {
+            pauseMenuWindow.close();
+            returnCall = true;
+        });
 
         addSettingsComponents();
         pauseLayout.getChildren().add(vBox);
         pauseMenuScene = new Scene( pauseLayout, SETTINGS_WINDOW_WIDTH, SETTINGS_WINDOWS_HEIGHT);
         pauseMenuWindow.setScene( pauseMenuScene);
+    }
+
+    private void changeVolume( int volume) {
+        this.volume = volume;
+        volumeBar.setValue( volume);
+    }
+
+    @Override
+    public void handle(ActionEvent event) {
+        if( event.getSource() == returnButton) {
+            // should return to the game screen
+        }
+    }
+
+    private void initSlider() {
+        volumeBar = new Slider();
+        volumeBar.setMin(0);
+        volumeBar.setMax(100);
+        volumeBar.setValue(volume);
+
+        volumeBar.setShowTickLabels( true);
+        volumeBar.setShowTickMarks( true);
+        volumeBar.setMajorTickUnit( 50);
+        volumeBar.setBlockIncrement( 10);
     }
 
     public void addSettingsComponents(){
@@ -64,7 +104,5 @@ public class PauseMenu {
     public void closeSettings(){
         pauseMenuWindow.close();
     }
-
-
 
 }
