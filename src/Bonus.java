a84397d6afe47e4d00a2f2daae0a5da5f8696272
mahinkhankishizade <mahinkhankishizade@gmail.com>
2017-12-13 13:26:25 +0300

    update

--- src/Bonus.java
+++ src/Bonus.java
@@ -1,8 +1,29 @@
 public class Bonus extends GameObject {
 
     private int typeID; // 0 = lifeBonus, 1 = speedBonus
+    private int releaseTime;
+    private int effectDuration;
+    private boolean released;
+    private boolean taken;
+    private boolean timeOut;
 
     public Bonus( int xLoc, int yLoc) {
         super(xLoc, yLoc);
+    }
+
+    private boolean isReleased() {
+        return released = true;
+    }
+
+    private boolean isTaken() {
+        return taken = true;
+    }
+
+    private boolean isTimeOut() {
+        return timeOut = true;
+    }
+
+    private void applyBonus(Player player) {
+        player.setHealth(player.getHealth() + 100);
     }
 }
